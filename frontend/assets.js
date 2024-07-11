67f63e8ab5b51a9f0d3a222e1260acd5b1bb2fd2
Fazin Faizal <fazinkarat@gmail.com>
2024-07-11 13:02:37 +0530

    updated asset table

--- frontend/assets.js
+++ frontend/assets.js
@@ -5,18 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(response => response.json())
     .then(items => {
         if (items.length === 0) {
-            assetsList.innerHTML = '<p>No assets available. Add new items <a href="item.html">here</a>.</p>';
+            assetsList.innerHTML = '<tr><td colspan="4">No assets available. Add new items <a href="item.html">here</a>.</td></tr>';
         } else {
+            let serialNumber = 1;
             items.forEach(item => {
                 if (item.type === 'assets') {
-                    const itemDiv = document.createElement('div');
-                    itemDiv.classList.add('card');
-                    itemDiv.innerHTML = `
-                        <h2>${item.name}</h2>
-                        <p>Model Number: ${item.model}</p>
-                        <p>Date: ${item.date}</p>
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                        <td>${serialNumber++}</td>
+                        <td>${item.name}</td>
+                        <td>${item.model}</td>
+                        <td>${item.date}</td>
                     `;
-                    assetsList.appendChild(itemDiv);
+                    assetsList.appendChild(row);
                 }
             });
         }
